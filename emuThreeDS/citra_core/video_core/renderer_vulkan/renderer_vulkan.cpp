c3feb9a52050f3772fb37e5f4317b013610181ed
Joseph Mattiello <git@joemattiello.com>
2025-03-14 02:42:13 -0400

    renderer vulkan fix metal warnings
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- emuThreeDS/citra_core/video_core/renderer_vulkan/renderer_vulkan.cpp
+++ emuThreeDS/citra_core/video_core/renderer_vulkan/renderer_vulkan.cpp
@@ -370,10 +370,18 @@ void RendererVulkan::BuildPipelines() {
         .pVertexAttributeDescriptions = attributes.data(),
     };
 
+    // Metal doesn't support disabling primitive restart, so we enable it on Apple platforms
+#if defined(__APPLE__)
+    const vk::PipelineInputAssemblyStateCreateInfo input_assembly = {
+        .topology = vk::PrimitiveTopology::eTriangleStrip,
+        .primitiveRestartEnable = true,
+    };
+#else
     const vk::PipelineInputAssemblyStateCreateInfo input_assembly = {
         .topology = vk::PrimitiveTopology::eTriangleStrip,
         .primitiveRestartEnable = false,
     };
+#endif
 
     const vk::PipelineRasterizationStateCreateInfo raster_state = {
         .depthClampEnable = false,
