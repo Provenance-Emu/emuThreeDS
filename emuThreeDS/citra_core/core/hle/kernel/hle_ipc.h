0bd565ca2cbc4cc9ee7350383a088babda960bb5
Joseph Mattiello <git@joemattiello.com>
2025-03-20 22:28:16 -0400

    hle_ipc.h start to add async kernel support
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- emuThreeDS/citra_core/core/hle/kernel/hle_ipc.h
+++ emuThreeDS/citra_core/core/hle/kernel/hle_ipc.h
@@ -242,10 +242,81 @@ public:
                                              std::chrono::nanoseconds timeout,
                                              std::shared_ptr<WakeupCallback> callback);
 
+//    private:
+//        template <typename ResultFunctor>
+//        class AsyncWakeUpCallback : public WakeupCallback {
+//        public:
+//            explicit AsyncWakeUpCallback(KernelSystem& kernel, ResultFunctor res_functor,
+//                                         std::future<void> fut)
+//                : kernel(kernel), functor(res_functor) {
+//                future = std::move(fut);
+//            }
+//
+//            void WakeUp(std::shared_ptr<Kernel::Thread> thread, Kernel::HLERequestContext& ctx,
+//                        Kernel::ThreadWakeupReason reason) override {
+//                functor(ctx);
+//                kernel.ReportAsyncState(false);
+//            }
+//
+//            bool SupportsSerialization() override {
+//                return false;
+//            }
+//
+//        private:
+//            KernelSystem& kernel;
+//            ResultFunctor functor;
+//            std::future<void> future;
+//        };
+//
+//    public:
+//        /**
+//         * Puts the game thread to sleep and calls the specified async_section asynchronously.
+//         * Once the execution of the async section finishes, result_function is called. Use this
+//         * mechanism to run blocking IO operations, so that other game threads are allowed to run
+//         * while the one performing the blocking operation waits.
+//         * @param async_section Callable that takes Kernel::HLERequestContext& as argument
+//         * and returns the amount of nanoseconds to wait before calling result_function.
+//         * This callable is ran asynchronously.
+//         * @param result_function Callable that takes Kernel::HLERequestContext& as argument
+//         * and doesn't return anything. This callable is ran from the emulator thread
+//         * and can be used to set the IPC result.
+//         * @param really_async If set to false, it will call both async_section and result_function
+//         * from the emulator thread.
+//         */
+//        template <typename AsyncFunctor, typename ResultFunctor>
+//        void RunAsync(AsyncFunctor async_section, ResultFunctor result_function,
+//                      bool really_async = true) {
+//
+//            if (!Settings::values.deterministic_async_operations && really_async) {
+//                kernel.ReportAsyncState(true);
+//                this->SleepClientThread(
+//                    "RunAsync", std::chrono::nanoseconds(-1),
+//                    std::make_shared<AsyncWakeUpCallback<ResultFunctor>>(
+//                        kernel, result_function,
+//                        std::move(std::async(std::launch::async, [this, async_section] {
+//                            s64 sleep_for = async_section(*this);
+//                            this->thread->WakeAfterDelay(sleep_for, true);
+//                        }))));
+//
+//            } else {
+//                s64 sleep_for = async_section(*this);
+//                if (sleep_for > 0) {
+//                    kernel.ReportAsyncState(true);
+//                    auto parallel_wakeup = std::make_shared<AsyncWakeUpCallback<ResultFunctor>>(
+//                        kernel, result_function, std::move(std::future<void>()));
+//                    this->SleepClientThread("RunAsync", std::chrono::nanoseconds(sleep_for),
+//                                            parallel_wakeup);
+//                } else {
+//                    result_function(*this);
+//                }
+//            }
+//        }
+
     /**
      * Resolves a object id from the request command buffer into a pointer to an object. See the
      * "HLE handle protocol" section in the class documentation for more details.
      */
+
     std::shared_ptr<Object> GetIncomingHandle(u32 id_from_cmdbuf) const;
 
     /**
